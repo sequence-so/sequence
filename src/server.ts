d8fc2a53ae7c6e9357c852138e9c233edcda0072
Helson <helsont@gmail.com>
2021-04-20 17:51:43 -0400

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URI);
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
