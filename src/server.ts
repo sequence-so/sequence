8fe691c090528a829ec3369305d27fce2381863b
Helson <helsont@gmail.com>
2021-04-20 17:53:56 -0400

    update database env

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URI);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const app = express();
 
