7f15d6e2226a46516b30392f8a56c211a79478be
Helson <helsont@gmail.com>
2021-04-27 17:06:44 -0400

    fix ssl database config

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,21 +1,17 @@
 import { Sequelize, SequelizeOptions } from "sequelize-typescript";
 
-const sequelizeOpts: SequelizeOptions = {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+const options: SequelizeOptions = {
+  dialectOptions: process.env.DB_SSL
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
   logging: process.env.NODE_ENV === "local" ? true : false,
 };
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
-  process.env.NODE_ENV === "development" ||
-  process.env.NODE_ENV === "production"
-    ? sequelizeOpts
-    : {}
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, options);
 
 export default sequelize;
