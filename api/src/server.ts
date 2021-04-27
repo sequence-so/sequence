cb4a4e0751ec77219bade32ee7dcc5b679d5fda9
Helson <helsont@gmail.com>
2021-04-27 17:32:48 -0400

    fixing cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -17,14 +17,14 @@ const app = express();
 
 app.disable("x-powered-by");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 if (process.env.USE_SSL) {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
