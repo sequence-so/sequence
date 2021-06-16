efee4e223efa038a47b9218c4cded21c520263df
Helson <helsont@gmail.com>
2021-06-16 10:18:39 -0700

    database models

---
+++ packages/api/src/migrations/20210616055712-create_campaign_node_edge.js
@@ -0,0 +1,49 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("campaign_node_edge", {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
+      },
+      from: {
+        type: Sequelize.UUID,
+        references: {
+          model: "campaign_node",
+          key: "id",
+        },
+        allowNull: false,
+      },
+      to: {
+        type: Sequelize.UUID,
+        references: {
+          model: "campaign_node",
+          key: "id",
+        },
+        allowNull: false,
+      },
+      edgeKind: {
+        allowNull: false,
+        defaultValue: 0,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("campaign_node_edge");
+  },
+};
